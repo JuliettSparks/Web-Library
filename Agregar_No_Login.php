7bccba4903b968ed53aa32cfd2d88fdca949089a
Juliett Sparks Jones <32524167+JuliettSparks@users.noreply.github.com>
2019-11-25 00:45:49 -0600

    Add files via upload

---
+++ Agregar_No_Login.php
@@ -0,0 +1,38 @@
+<?php
+    $id=$_POST['id'];
+    $nombre=$_POST['nombre'];
+    $apellido_pat=$_POST['apellido_pat'];
+    $apellido_mat=$_POST['apellido_mat'];
+    $puesto=$_POST['puesto'];
+    //echo $puesto;
+    $pass=$_POST['password'];
+    if($nombre==""){
+        header("location: FailNotEnoughData.html");
+        die();
+    }
+$servidor="localhost";
+$usuario="root";
+$contrasena="";
+$db="biblioteca";
+
+$conexion=mysqli_connect($servidor,$usuario,$contrasena,$db);
+if(!$conexion){
+    echo "Error: No se puede conectar a la DB".PHP_EOL;
+    echo "Errno de depuracion ".mysqli_connect_errno().PHP_EOL;
+    echo "Error de depuracion ".mysqli_connect_error().PHP_EOL;
+    exit;
+}
+//Mandar llamar al Script que Validará la entrada del ID
+/*$row = mysql_fetch_array($result)*/
+$result = "SELECT * FROM personas WHERE id LIKE '$id'";
+$answer=mysqli_query($conexion,$result);
+if($row=mysqli_fetch_array($answer)){
+    header("location: Refuse.html");
+}else{
+$alta="INSERT INTO personas(id,nombre,apellido_pat,apellido_mat,puesto,pass) VALUES ('$id','$nombre','$apellido_pat','$apellido_mat','$puesto','$pass')";
+mysqli_query("SET NAMES 'utf8'");
+mysqli_query($conexion,$alta);
+mysqli_close($conexion);
+header("location: Success_AU_NoLogin.php");
+}
+?>
---
+++ Success_AU_No_Login.php
@@ -0,0 +1,22 @@
+﻿<?php
+/*
+    session_start();
+    $var_usuario=$_SESSION['id'];
+    if($var_usuario=="" || $var_usuario==null){
+        header("location: Refuse.html");
+    }*/
+?>
+<!DOCTYPE html>
+
+<html lang="es-mx" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Biblioteca</title>
+</head>
+<body>
+    <form action="NewUserByNoLoginMode.php" method="post">
+        <h2>Éxito Al Agregar al Nuevo Alumno</h2>
+        <input type="submit" name="panel1"value= "Regresar a la Página Principal">
+    </form>
+</body>
+</html>
